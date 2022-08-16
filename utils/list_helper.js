33ad80771381a2756e74023ecbc58c16051e2cd2
Ludovico <25531990+ludovico-chiavelli@users.noreply.github.com>
2022-08-16 15:30:32 +0100

    exercise 4.5 done. Write favouriteBlog function and tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const fBlog = blogs.reduce((max, blog) => max.likes > blog.likes ? max : blog)
+  return (
+    {
+      title: fBlog.title,
+      author: fBlog.author,
+      likes: fBlog.likes
+    }
+  )
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
